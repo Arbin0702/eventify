417bc6b3497c9d7cf105480fd835533f588c1f3d
Arbin Shiwakoti <arbint208@gmail.com>
2026-03-07 09:20:02 +1100

    remove root package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
